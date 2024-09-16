4949bf35801aef59fd446fdbd642785050215b3a
WangErica <53535628+WangErica@users.noreply.github.com>
2024-09-16 07:43:13 -0400

    homework

--- Week_2/Homework/src/App.js
+++ Week_2/Homework/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import RecipeCard from './RecipeCard'
 import './global.css'
+import Header from './components/header.js';
+import Wrapper from './components/wrapper.js';
 
 function App() {
-  return <RecipeCard />
-}
+  return (
+    <div>
+      <Header/>
+      <Wrapper><center>
+      <RecipeCard />
+      </center></Wrapper>
+  </div>
+)}
 
 export default App
